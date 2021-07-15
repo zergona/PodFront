6afe7f7e0ba694dd47e9fd73d052cadb9e08f233
Haris <h.muharemovic@gmail.com>
2021-07-15 11:33:40 +0200

    Fixing deployment

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/pod'));
+app.use(express.static('./dist/POD'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/pod/'}),
+    res.sendFile('index.html', {root: 'dist/POD/'}),
 );
 
 // Start the app by listening on the default Heroku port
